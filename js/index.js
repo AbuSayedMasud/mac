0568f5f2063f677750e2ff68dba534f0616246e4
AbuSayedMasud <sayedmasud1996@gmail.com>
2021-08-21 18:33:12 +0600

    final- push

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
+// choosing component
 function productCost(product, price) {
     const productInput = document.getElementById(product + '-cost')
 
     productInput.innerText = price;
     calculationOfTotal();
 }
+
 function getValue(product) {
     const priceString = document.getElementById(product + '-cost');
     const productPrice = parseInt(priceString.innerText);
     return productPrice;
 }
+// value calculation
 function calculationOfTotal() {
     const memoryPrice = getValue('memory');
     const StoragePrice = getValue('storage');
@@ -48,25 +51,27 @@ document.getElementById('delivaryon25Augest').addEventListener('click', function
 document.getElementById('delivaryon21Augest').addEventListener('click', function () {
     productCost('delivary', 20);
 })
+// promocode section
 document.getElementById('promo-code').addEventListener('click', function () {
-    const promoCode = document.getElementById('input-value');
+    var promoCode = document.getElementById('input-value');
 
     const bestPrice = document.getElementById('total-cost');
     const footerTotalPrice = document.getElementById('footer-total-cost');
-    const totalCost = parseInt(bestPrice.innerText);
-    const promoValue = promoCode.value;
+    const totalCost = parseFloat(bestPrice.innerText);
 
 
-    if (promoCode.value == 'stevekaku') {
+
+
+    if (promoCode.value === 'stevekaku') {
         const discount = (totalCost * 20) / 100;
         const afterUsingPromoCode = totalCost - discount;
         bestPrice.innerText = afterUsingPromoCode;
         footerTotalPrice.innerText = afterUsingPromoCode;
-        document.getElementById('input-value').value = ' ';
+        document.getElementById('input-value').value = '';
     }
     else {
         console.log("PromoCode is not current");
-        promoCode.value = ' ';
+        promoCode.value = '';
 
     }
 
